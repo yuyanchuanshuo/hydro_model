30d39902dca4ca86aadca30a76f30f1604e9d7ac
yuyanchuanshuo <727558985@qq.com>
2025-08-01 15:12:31 +0800

    20250801

--- oned_hydro_model/main.f90
+++ oned_hydro_model/main.f90
@@ -1,6 +1,6 @@
 program main
     implicit none
-    integer :: i, j, number, t_all, dt, t
+    integer :: i, j, number, t_all, dt, t,j_out
     real, dimension(:, :), allocatable :: A, Q, Q0, A0, H, R
     real, dimension(:), allocatable :: n, dx, ZB
     integer :: file_unit  ! 添加文件单元号变量
@@ -36,7 +36,7 @@ program main
     ! 新增：输出到文件
     open(newunit=file_unit, file='data_output/section_hydraulic_properties.txt', status='replace', action='write')
     write(file_unit, '(A)') '断面ID 水位(m) 面积(m2) 水面宽度(m) 湿周(m) 水力半径(m)'
-    
+
     do i = 1, n_sections
         do j = 1, size(section_hydraulics(i)%results, 1)
             write(file_unit, '(A10, 5F15.6)') trim(section_hydraulics(i)%section_id), &
@@ -49,9 +49,8 @@ program main
     enddo
     close(file_unit)
 
-
     number = n_sections !假设有100个断面，待修改
-    t_all = 100000 !计算总时段
+    t_all = 100 !计算总时段
     dt = 3 ! 计算间隔10s
 
     ! 分配数组内存
@@ -67,11 +66,11 @@ program main
     n = 0.035 ! 假设每一个断面的糙率均为0.035
 
     do i = 2, number
-        if (i==1) then
+        if(i == 1) then
             dx(i) = 0
             ZB(i) = minval(sections(i)%elevations)
         else
-            dx(i) = abs(sections(i)%start_distance - sections(i-1)%start_distance)
+            dx(i) = abs(sections(i)%start_distance - sections(i - 1)%start_distance)
             ZB(i) = minval(sections(i)%elevations)
         endif
     enddo
@@ -81,12 +80,16 @@ program main
         Q(1, i) = 100 + (150 - 100) * real(i - 1) / (t_all - 1)
     enddo
 
+    j_out = 100
     do t = 1, t_all - 1
+
         call dis_cal(number, n, dx, dt, Q0(:, t), ZB, A(:, t), Q(:, t + 1), H(:, t))  ! 使用call调用子例程
         Q0(2:number, t + 1) = Q(2:number, t + 1) ! 定义这个断面输入的Q
         Q(1, t + 1) = Q0(1, t + 1)
         A(:, t + 1) = area_cal(number, dt, Q(:, t + 1), A0(:, t), dx)
         A0(:, t + 1) = A(:, t + 1) ! 定义这个断面输入的A
+
+        print *, '第', t, '时刻', j_out, '个断面的流量为：', Q(j_out, t), A(j_out, t), H(j_out, t)
 
     enddo
 
@@ -131,16 +134,19 @@ contains
     subroutine dis_cal(n_size, n_in, dx_in, dt_in, Q_in, ZB_in, A_in, Q_out, h_out) !函数只能返回一个结果变量，改用子例程
         implicit none
         integer :: iii
-        integer :: B = 50 ! 假设一个何宽，做试算，后期要删掉
         integer, intent(in) :: n_size, dt_in
         real, intent(in), dimension(n_size) :: dx_in, n_in
-        real, dimension(n_size) :: uu, Q_in, H_in, ZB_in, A_in, R_in
+        real, dimension(n_size) :: uu, Q_in, H_in, ZB_in, A_in, R_in, B, wetted_perimeter,ZZ
         real, dimension(n_size) :: Q_out, h_out
         real :: XDX, HDX, FRI, alfa = 1 !alfa动量修正系数，一般取1
 
-        H_in = A_in / B ! 假设一个何宽，做试算，后期要删掉
+        Do iii = 1, n_size
+            call find_hydraulic_properties(section_hydraulics(iii)%results, A_in(iii), ZZ(iii), B(iii), wetted_perimeter(iii), R_in(iii))
+            H_in(iii) = ZZ(iii) - ZB_in(iii)
+        enddo
+
         h_out = H_in
-        R_in = A_in / (2 * H_in + B) ! 假设一个何宽，做试算，后期要删掉，水力半径 = 过水面积/湿周
+
         uu(1) = Q_in(1) / A_in(1)
 
         Do iii = 2, n_size
@@ -169,14 +175,14 @@ contains
 
         enddo
 
-    endsubroutine dis_cal ! 子例程不需要返回值，所以去掉result
+    endsubroutine dis_cal 
 
     ! 添加断面数据读取子程序
     subroutine read_section_data(filename, sections_out, n_sections_out)
         character(len=*), intent(in) :: filename
         type(section_data), dimension(:), allocatable, intent(out) :: sections_out
         integer, intent(out) :: n_sections_out
-        integer :: ios, section_num, ii, jj,n_points
+        integer :: ios, section_num, ii, jj, n_points
         integer :: total_lines = 0 ! 文件总行数
         character(len=256) :: line, start_distance_str
         integer, dimension(:), allocatable :: id_line_numbers  ! 新增：存储ID行行号
@@ -217,7 +223,7 @@ contains
         jj = 0
         ! 第二遍读取：逐行读取，判断是ID行，n就+1
         do ii = 1, total_lines
-            
+
             if(ii == id_line_numbers(section_num + 1)) then
                 if(ii /= 1) then
                     ! 调整数组大小为实际读取的点数
@@ -262,7 +268,7 @@ contains
         ! 新增：输出断面详细数据到文件
         open(newunit=file_unit, file='data_output/section_details.txt', status='replace', action='write')
         write(file_unit, '(A)') '断面序号 断面ID 起点距(m) 点号 距离(m) 高程(m)'
-        
+
         do ii = 1, n_sections_out
             do jj = 1, size(sections_out(ii)%distances)
                 write(file_unit, '(I10, A10, F15.3, I10, 2F15.3)') ii, trim(sections_out(ii)%id), &
@@ -273,7 +279,6 @@ contains
             enddo
         enddo
         close(file_unit)
-
 
     endsubroutine read_section_data
 
@@ -290,45 +295,45 @@ contains
         n_points_in = size(section_in%distances)
         min_elev = minval(section_in%elevations)
         max_elev = maxval(section_in%elevations)
-        
+
         ! 设置水位范围：从最低点到最高点+10m，步长0.1m
         n_levels = int((max_elev + 10.0 - min_elev) / 0.1) + 1
         allocate(results_matrix(n_levels, 5))  ! 4列：水位、面积、宽度、湿周、水力半径
         allocate(areas(n_levels))
         allocate(widths(n_levels))
         allocate(wetted_perimeters(n_levels))
-        
+
         ! 初始化水位数组
         do iii = 1, n_levels
-            results_matrix(iii, 1) = min_elev + (iii-1)*0.1
+            results_matrix(iii, 1) = min_elev + (iii - 1) * 0.1
         enddo
-        
+
         ! 计算每个水位对应的水力特性
         do iii = 1, n_levels
             current_h = results_matrix(iii, 1)
             areas(iii) = 0.0
             widths(iii) = 0.0
             wetted_perimeters(iii) = 0.0
-            
+
             ! 遍历所有断面点
             do jjj = 2, n_points_in
-                left_dist = section_in%distances(jjj-1)
+                left_dist = section_in%distances(jjj - 1)
                 right_dist = section_in%distances(jjj)
-                left_elev = section_in%elevations(jjj-1)
+                left_elev = section_in%elevations(jjj - 1)
                 right_elev = section_in%elevations(jjj)
-                
+
                 ! 计算当前水位下的有效宽度
-                if (current_h > min(left_elev, right_elev)) then
-                    segment_width = right_dist - left_dist   
+                if(current_h > min(left_elev, right_elev)) then
+                    segment_width = right_dist - left_dist
 
                     ! 计算梯形面积
-                    if (current_h >= max(left_elev, right_elev)) then
-                        segment_area = segment_width * (current_h - (left_elev + right_elev)/2.0)
+                    if(current_h >= max(left_elev, right_elev)) then
+                        segment_area = segment_width * (current_h - (left_elev + right_elev) / 2.0)
                         segment_perimeter = segment_width + &
-                            sqrt((right_dist - left_dist)**2 + (right_elev - left_elev)**2)
+                                            sqrt((right_dist - left_dist)**2 + (right_elev - left_elev)**2)
                     else
                         ! 部分淹没情况(三角形)
-                        if (left_elev < right_elev) then
+                        if(left_elev < right_elev) then
                             segment_width = segment_width * (current_h - left_elev) / (right_elev - left_elev)
                             segment_area = 0.5 * segment_width * (current_h - left_elev)
                             segment_perimeter = sqrt(segment_width**2 + (current_h - left_elev)**2)
@@ -343,23 +348,47 @@ contains
                     wetted_perimeters(iii) = wetted_perimeters(iii) + segment_perimeter
                 endif
             enddo
-            
+
             ! 填充结果矩阵
             results_matrix(iii, 2) = areas(iii)                 ! 面积
             results_matrix(iii, 3) = widths(iii)                ! 水面宽度
             results_matrix(iii, 4) = wetted_perimeters(iii)     ! 湿周
 
             ! 计算水力半径
-            if (wetted_perimeters(iii) > 0.0) then
+            if(wetted_perimeters(iii) > 0.0) then
                 results_matrix(iii, 5) = areas(iii) / wetted_perimeters(iii)
             else
                 results_matrix(iii, 5) = 0.0
             endif
         enddo
-        
+
         deallocate(areas, widths, wetted_perimeters)
 
-
     endsubroutine calculate_hydraulic_properties
+
+    ! 修改后的查找函数，直接使用已计算的水力特性数据
+    subroutine find_hydraulic_properties(result_in, A_in, h_out, B_out, wp_out, R_out)
+        real, dimension(:, :), allocatable, intent(in) :: result_in
+        real, intent(in) :: A_in
+        real, intent(out) :: h_out, B_out, wp_out, R_out
+        integer :: ii
+
+        ! 直接使用已计算的水力特性结果
+        do ii = 1, size(result_in, 1) - 1
+            if(result_in(ii, 2) <= A_in .and. &
+               result_in(ii + 1, 2) >= A_in) then
+                ! 线性插值
+                h_out = result_in(ii, 1) + (A_in - result_in(ii, 2)) * (result_in(ii + 1, 1) - result_in(ii, 1)) / &
+                              (result_in(ii + 1, 2) - result_in(ii, 2))
+                B_out = result_in(ii, 3) + (A_in - result_in(ii, 2)) * (result_in(ii + 1, 3) - result_in(ii, 3)) / &
+                        (result_in(ii + 1, 2) - result_in(ii, 2))
+                wp_out = result_in(ii, 4) + (A_in - result_in(ii, 2)) *  (result_in(ii + 1, 4) - result_in(ii, 4)) / &
+                                   (result_in(ii + 1, 2) - result_in(ii, 2))
+                R_out = result_in(ii, 5) + (A_in - result_in(ii, 2)) * (result_in(ii + 1, 5) - result_in(ii, 5)) / &
+                                   (result_in(ii + 1, 2) - result_in(ii, 2))
+                exit
+            endif
+        enddo
+    endsubroutine find_hydraulic_properties
 
 endprogram main
